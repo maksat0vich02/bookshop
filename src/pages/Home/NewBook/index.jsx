a5e76f3ee10ccdba8c0a3076094bf57238a01f82
Beksultan <147965986+maksat0vich02@users.noreply.github.com>
2024-02-13 18:56:41 +0600

    Book shop header (#2)
    * bookshop
    
    * HomeBook

---
+++ src/pages/Home/NewBook/index.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+
+const NewBook = () => {
+  return (
+    <>
+      <section id="newBook">
+        <div className="container">
+          <div className="newBook">
+            <h1>New Books</h1>
+          </div>
+          <div className="newBookBlock">
+            <div className="newBookBox">
+              <img
+                src="https://s3-alpha-sig.figma.com/img/39ab/cd26/d479e63c7133e8c339599cf5fb21f869?Expires=1708905600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=mcF~LEOWsCdBnFCMIydBr-h8OuIKsmOrVBKmjakaA9~G9t9NB1K~QAKA2B7M9LEXV4LeheUS9hsUduQdQtsQ8G9UFqEbWevVGfNRkKM2WN~swvVx-IQdUufFdz-MtGDAevt3DgnNxUe1LR0zjrcTsId4c51xcxkBzBIDnIPh7qNiAXMrIkEZFcsucttgQ5r2kTBPWG0-7V8E0KzYe46l0wHStvHqyh2cPtr2gFg8K4DDsBPL8KMkWn9DccK74baH4hLLTz7zrwkzLHFzz5PVnU~Q5IYcBdxnhPHlYuQHgMTkvXIUCnrqPYyDkKQ~mqvt~1XnDH~7TTHCjsr7DRiZgw__"
+                alt=""
+              />
+              <h3>
+                The Climate Book: <br /> The Facts and the <br /> Solutions
+              </h3>
+              <p>by Greta Thunberg</p>
+            </div>
+            <div className="newBookBoxx">
+              <img
+                src="https://s3-alpha-sig.figma.com/img/44bf/b92a/df318f90109ac54a78d55bc98e359268?Expires=1708905600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=gKOILZGaGhyHahuS8BGRg1vrt94EcQWzFGG2rbKs1QBhx5FYNLvbLEv3AT8N8Zal~VeiuuD7~aUopKDBTOEYiCeupGqAPbaNaT094PUCdD0WxECzNRIh8tzS0VOxiz0cP-hRdb0GCrwFevEx2ab0fUi3fI8-mY3DwCI9u7eFYMV7-e2khNPu07rudrqfKHamEwibbZH5NvXw7n7kR2pewYsSWMvwwLQFWrV5fKmfNfb8yLRg~EJWxYbTMHOboz1AFyTbNx2fXVtNxz8P1cqt57U3n6q1uIA3Bq6SXiP0esk9M-UTHb4~ZzXfkoNQItn2cIlUIVLtdu8W7IPDPSG~Qg__"
+                alt=""
+              />
+              <h3>
+                Rest Is Resistance: <br /> A Manifesto
+              </h3>
+              <p>by Tricia Hersey</p>
+            </div>
+            <div className="newBookBox">
+              <img
+                src="https://s3-alpha-sig.figma.com/img/7e00/e90b/6fb5c80767c52826306bdfd0ad94938c?Expires=1708905600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=oT7Usk6IwewXxpOiWL5c-exXTGKGxDTtQGstAKBxjr5aj0YDUI56DErbZiZo~ugJtGsxn50DLfG4tr1AhhMSKee34X8~HOmlkvN-Wpwm9NhAF10-~lHutKW5S1uoxOWWIVvOqmn4oaQS9CWP~XVD9U0rKPgI8VSszH7TWNb29jMlbzPyqRztbvAhn6uFA4TFi2NOJv2Rs5G7WmvKu-axQt8yY~9wBO7Ec7YBitCDD05sADY99N4o95vYvp52kOaVpMcYsVLRLSLewVheClR9tNAV6gXOfVFlto8cj9lfvTxB5lTVNhudv-giX-oVYtfbl1UoyhbAhfZdAef51CfPEA__"
+                alt=""
+              />
+              <h3>
+                A New Name: <br /> Septology VI-VII
+              </h3>
+              <p>by Jon Fosse</p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default NewBook;
